fd7f9b5b733f7d0a33164bb09a78f0451b8b3cad
bossoncode <akinkunmiisrael41@gmail.com>
2023-04-10 16:32:40 +0100

    ___

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-interface CustomNextApiRequest extends NextApiRequest {
-    method: string;
-}
-export declare function allowMethods(methods: string[]): (handler: NextApiHandler) => (req: CustomNextApiRequest, res: NextApiResponse) => unknown;
-export {};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+interface CustomNextApiRequest {
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
+  url: string;
+  headers: { [key: string]: string };
+  query: { [key: string]: string | string[] };
+  body?: any;
+}
 
-interface CustomNextApiRequest extends NextApiRequest {
-  method: string;
+interface CustomNextApiResponse {
+  status: (statusCode: number) => CustomNextApiResponse;
+  json: (data: any) => void;
 }
 
 export function allowMethods(methods: string[]) {
-  return (handler: NextApiHandler) =>
-    (req: CustomNextApiRequest, res: NextApiResponse) => {
+  return (
+      handler: (req: CustomNextApiRequest, res: CustomNextApiResponse) => void
+    ) =>
+    (req: CustomNextApiRequest, res: CustomNextApiResponse) => {
       if (!methods.includes(req.method)) {
         res.status(405).json({
           error: "Method Not Allowed",
